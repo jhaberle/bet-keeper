abef936c6c8f4b796de4882d7d6802401b766478
Jake Haberle <jake.haberle@gmail.com>
2020-11-12 21:12:10 -0700

    created files and rendered app to screen

---
+++ betkeeper/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+const StyledFooter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #ffffff;
+  width: 100%;
+  height: calc(100vh - 90vh);
+  padding: 20px;
+`;
+const Footer = () => {
+  return (
+    <StyledFooter>
+      &copy;{new Date().getFullYear()} Bet Keeper, Where drinking and betting
+      becomes one.
+    </StyledFooter>
+  );
+};
+export default Footer;
---
+++ betkeeper/src/utils/betApi.js
@@ -0,0 +1,1 @@
+// API to get odds back
