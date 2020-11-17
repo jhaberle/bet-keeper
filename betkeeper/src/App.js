0686b45de11fb4d6549b26f8860f680a61abace5
Jake Haberle <jake.haberle@gmail.com>
2020-11-17 16:52:16 -0700

    added login, routing and bets page

--- betkeeper/src/App.js
+++ betkeeper/src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
 import GeneralContainer from "./components/GeneralContainer";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Members from "./pages/Members";
+import NavBar from "./components/NavBar";
+import Layout from "./components/Layout";
+import Login from "./components/Login";
 
 function App() {
-  return <GeneralContainer />;
+  return (
+    <React.Fragment>
+      <NavBar />
+      <Layout>
+        <Router>
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/members" component={GeneralContainer} />
+            <Route exact path="/bets" component={Members} />
+          </Switch>
+        </Router>
+      </Layout>
+    </React.Fragment>
+  );
 }
 export default App;
--- betkeeper/src/components/NavBar.js
+++ betkeeper/src/components/NavBar.js
@@ -1,32 +1,23 @@
-import React from 'react';
-import styled from 'styled-components';
-import Nav from 'react-bootstrap/Nav';
+import React from "react";
+import styled from "styled-components";
+import { Navbar, Nav } from "react-bootstrap";
 
 const StyledNavBar = styled.div`
-    .navcss {
-        background-color: black;
-    }
+  .navcss {
+    background-color: black;
+  }
 `;
 
 const NavBar = () => {
-
-    return (
-<StyledNavBar>       
-    <Nav className="navcss" activeKey="/home" onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}>
-        <Nav.Item>
-            <Nav.Link href="/home">Profile</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-            <Nav.Link eventKey="link-1">Link</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-        <Nav.Link eventKey="disabled" disabled>Bet Keeper</Nav.Link>
-        </Nav.Item>
-    </Nav>
-</StyledNavBar>
-    );
-
-
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="/login">Bet Keeper</Navbar.Brand>
+      <Nav className="navbar-links">
+        <Nav.Link href="/members">Members</Nav.Link>
+        <Nav.Link href="/bets">Bets</Nav.Link>
+      </Nav>
+    </Navbar>
+  );
 };
 
 export default NavBar;
