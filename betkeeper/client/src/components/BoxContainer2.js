4f372b365ad3ae6dcd9a37673295c779e79628e3
Jake Haberle <59548398+jhaberle@users.noreply.github.com>
2020-11-24 18:29:51 -0700

    deleted dupe node modules

--- betkeeper/client/src/components/BoxContainer2.js
+++ betkeeper/client/src/components/BoxContainer2.js
@@ -6,49 +6,51 @@ import Container from "react-bootstrap/Container";
 import MatchSearchForm from "./MatchSearchForm";
 
 const StyledBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid black;
+  width: 60vh;
+  margin-top: 50px;
+  height: fit-content;
+  border-radius: 15px;
+  flex-direction: column;
+  background: linear-gradient(
+      to bottom,
+      rgba(255, 255, 255, 0.15) 0%,
+      rgba(0, 0, 0, 0.15) 100%
+    ),
+    radial-gradient(
+        at top center,
+        rgba(255, 255, 255, 0.4) 0%,
+        rgba(0, 0, 0, 0.4) 120%
+      )
+      #989898;
+  background-blend-mode: multiply, multiply;
+  box-shadow: 12px 12px 2px 1px rgba(0, 0, 255, 0.2);
 
-display: flex;
-justify-content: center;
-align-items: center;
-border: 2px solid black; 
-width: 60vh;
-<<<<<<< HEAD
-margin-top: 50px;
-=======
-
-margin-top: 50px;
-
->>>>>>> b11372ff79bd4d18ea0b109fbd37684164c60c7b
-height: fit-content;
-border-radius: 15px;
-flex-direction: column;
-background: linear-gradient(to bottom, rgba(255,255,255,0.15) 0%, rgba(0,0,0,0.15) 100%), radial-gradient(at top center, rgba(255,255,255,0.40) 0%, rgba(0,0,0,0.40) 120%) #989898;
-background-blend-mode: multiply,multiply;
-box-shadow: 12px 12px 2px 1px rgba(0, 0, 255, .2);
-
-
-    h1 {
-        color: black;
-    }
+  h1 {
+    color: black;
+  }
 `;
 
 const Box2 = () => {
-
-    
-    return (
-
-        <Container fluid>
-            <StyledBox>
-                <Row>
-                    <Col><MatchSearchForm /></Col>
-                </Row>
-                <Row>
-                    <Col><p id="randomres">RESPONSE GOES HERE</p></Col>
-                </Row>
-            </StyledBox>
-        </Container>
-    );
-
+  return (
+    <Container fluid>
+      <StyledBox>
+        <Row>
+          <Col>
+            <MatchSearchForm />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <p id="randomres">RESPONSE GOES HERE</p>
+          </Col>
+        </Row>
+      </StyledBox>
+    </Container>
+  );
 };
 
 export default Box2;
--- betkeeper/controllers/betslipsController.js
+++ betkeeper/controllers/betslipsController.js
@@ -1,16 +1,14 @@
 const db = require("../models");
 
 module.exports = {
-    findAll: function(req, res) {
-        db.Betslip
-            .find(req.query)
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
-    },
-    create: function(req, res) {
-        db.Betslip
-            .create(req.body)
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
-    }
+  findAll: function (req, res) {
+    db.Betslip.find(req.query)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  create: function (req, res) {
+    db.Betslip.create(req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
 };
