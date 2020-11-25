a5cb1a5d2d20cd015b5cd5d5d7d99bee2be79287
Jake Haberle <59548398+jhaberle@users.noreply.github.com>
2020-11-24 19:34:29 -0700

    deleted node modules

--- betkeeper/server.js
+++ betkeeper/server.js
@@ -10,9 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/"));
+  app.use(express.static("client/build"));
 }
-app.use(express.static());
+
 // Add routes, both API and views
 app.use(routes);
 
